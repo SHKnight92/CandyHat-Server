5215579cd7875c8cc77362e0eb63e5bec749781f
Saddam hossain knight <knight.ru.bd.com>
2021-04-30 23:33:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const { ObjectID } = require("bson");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Welcome to Bazar-Sodai Server");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hzakv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productCollection = client.db("CandyHat").collection("products");
+
+  app.post("/addProduct", (req, res) => {
+    const newProduct = req.body;
+    productCollection.insertOne(newProduct).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/products", (req, res) => {
+    productCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+  app.get("/product/:id", (req, res) => {
+    productCollection
+      .find({ _id: ObjectID(req.params.id) })
+      .toArray((err, items) => {
+        res.send(items);
+      });
+  });
+
+  app.delete("/deleteProduct/:id", (req, res) => {
+    productCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+
+  const orderCollection = client.db("CandyHat").collection("orders");
+
+  app.post("/addOrder", (req, res) => {
+    const newOrder = req.body;
+    orderCollection.insertOne(newOrder).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/orders", (req, res) => {
+    orderCollection.find({ email: req.query.email }).toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  app.delete("/deleteOrder/:id", (req, res) => {
+    orderCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+
+  // perform actions on the collection object
+  //   client.close();
+});
+
+app.listen(process.env.PORT || 5055, (err) => {
+  console.log("Listening");
+});
